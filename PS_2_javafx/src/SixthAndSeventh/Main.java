e6d558c8b0a0aeb6aef7063a30f7c3aecea3a38c
BermetBurkanova <34917180+BermetBurkanova@users.noreply.github.com>
2021-04-08 00:09:21 +0600

    Add files via upload

---
+++ PS_2_javafx/src/SixthAndSeventh/Main.java
@@ -0,0 +1,22 @@
+package SixthAndSeventh;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Main");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
