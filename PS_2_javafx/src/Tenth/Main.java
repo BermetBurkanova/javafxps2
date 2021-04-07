f6a3e8496c1060088d363e75e21451cc10cf8449
BermetBurkanova <34917180+BermetBurkanova@users.noreply.github.com>
2021-04-08 01:18:42 +0600

    Add files via upload

---
+++ PS_2_javafx/src/Tenth/Main.java
@@ -0,0 +1,24 @@
+package Tenth;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root =
+                FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Draw shapes with Canvas");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
